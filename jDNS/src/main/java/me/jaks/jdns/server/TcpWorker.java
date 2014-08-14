bc5e6e2d856fe1abcf19b8602354cdab3b4531ec
Jacky Lam <jwhlam@gmail.com>
2014-08-13 22:39:35 -0600

    Converted getRecord calls to static methods

--- jDNS/src/main/java/me/jaks/jdns/server/TcpWorker.java
+++ jDNS/src/main/java/me/jaks/jdns/server/TcpWorker.java
@@ -75,7 +75,7 @@ public class TcpWorker implements Runnable{
 			
 			switch(type) {
 				case 1: // A Record
-					ARecord[] result = getARecord(domain);
+					ARecord[] result = RecordRepo.getARecord(domain,ds);
 					if(result == null) {
 						counter.setNxdomain();
 						ByteArrayOutputStream rbufStream = new ByteArrayOutputStream();
@@ -156,7 +156,7 @@ public class TcpWorker implements Runnable{
 					break;
 						
 				case 33:
-					SRVRecord[] srvResult = getSRVRecord(domain);
+					SRVRecord[] srvResult = RecordRepo.getSRVRecord(domain,ds);
 					if(srvResult == null) {
 						counter.setNxdomain();
 						ByteArrayOutputStream rbufStream = new ByteArrayOutputStream();
@@ -236,7 +236,7 @@ public class TcpWorker implements Runnable{
 					}
 					break;
 				case 35:
-					NAPTRRecord[] naptrResult = getNAPTRRecord(domain);
+					NAPTRRecord[] naptrResult = RecordRepo.getNAPTRRecord(domain,ds);
 					if(naptrResult == null) {
 						counter.setNxdomain();
 						ByteArrayOutputStream rbufStream = new ByteArrayOutputStream();
@@ -343,142 +343,6 @@ public class TcpWorker implements Runnable{
 		}
 	}
 
-private ARecord[] getARecord(String name) {
-		
-		boolean found = false;
-		Connection conn = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		ArrayList<ARecord> aList = new ArrayList<ARecord>();
-		
-		try {
-			conn = ds.getConnection();
-			st = conn.prepareStatement("SELECT ttl,content,disabled from records where name = ? and type = ?");
-			st.setString(1, name);
-			st.setString(2, "a");
-			rs = st.executeQuery();
-			while(rs.next()) {
-				long ttl = rs.getLong(1);
-				String ipaddress = rs.getString(2);
-				boolean disabled = rs.getBoolean(3);
-				ARecord temp = new ARecord(name,ttl,ipaddress,disabled);
-				found = true;
-				aList.add(temp);
-			}
-			st.close();
-			rs.close();
-			
-		} catch (SQLException e) {
-			logger.error("",e);
-		}
-		finally {
-			JDBChelper.close(rs);
-			JDBChelper.close(st);
-			JDBChelper.close(conn);
-			
-		}
-			if(found == true)
-				return aList.toArray(new ARecord[aList.size()]);
-			else
-				return null;
-	}
-	
-	private SRVRecord[] getSRVRecord(String name) {
-			
-			boolean found = false;
-			Connection conn = null;
-			PreparedStatement st = null;
-			ResultSet rs = null;
-			ArrayList<SRVRecord> aList = new ArrayList<SRVRecord>();
-			
-			try {
-				conn = ds.getConnection();
-				st = conn.prepareStatement("SELECT ttl,priority,content,disabled from records where name = ? and type = ?");
-				st.setString(1, name);
-				st.setString(2,"srv");
-				rs = st.executeQuery();
-				while(rs.next()) {
-					long ttl = rs.getLong(1);
-					int rrclass = 1;
-					int priority = rs.getInt(2);
-					String content = rs.getString(3);
-					boolean disabled = rs.getBoolean(4);
-					String[] data = content.split("[ ]+");
-					int weight = Integer.parseInt(data[0]);
-					int port = Integer.parseInt(data[1]);
-					String target = data[2];
-					SRVRecord temp = new SRVRecord(name,ttl,rrclass,priority,weight,port,target,disabled);
-					found = true;
-					aList.add(temp);
-				}
-				st.close();
-				rs.close();
-				
-			} catch (SQLException e) {
-				logger.error("",e);
-			}
-			finally {
-				JDBChelper.close(rs);
-				JDBChelper.close(st);
-				JDBChelper.close(conn);
-				
-			}
-				if(found == true)
-					return aList.toArray(new SRVRecord[aList.size()]);
-				else
-					return null;
-		}
-	
-	private NAPTRRecord[] getNAPTRRecord(String name) {
-		
-		boolean found = false;
-		Connection conn = null;
-		PreparedStatement st = null;
-		ResultSet rs = null;
-		ArrayList<NAPTRRecord> aList = new ArrayList<NAPTRRecord>();
-		
-		try {
-			conn = ds.getConnection();
-			
-			
-			st = conn.prepareStatement("SELECT ttl,content,disabled from records where name = ? and type = ?");
-			st.setString(1, name);
-			st.setString(2,"naptr");
-			rs = st.executeQuery();
-	
-			while(rs.next()) {
-				long ttl = rs.getLong(1);
-				int rrclass = 1;
-				String content = rs.getString(2);
-				boolean disabled = rs.getBoolean(3);
-				String[] data = content.split("[ ]+");
-				int order = Integer.parseInt(data[0]);
-				int pref = Integer.parseInt(data[1]);
-				String flags = data[2].replaceAll("\"", "");
-				String services = data[3].replaceAll("\"", "");
-				String regex = data[4].replaceAll("\"","");
-				String replace = data[5];
-				NAPTRRecord temp = new NAPTRRecord(name,ttl,rrclass,order,pref,flags,services,regex,replace,disabled);
-				found = true;
-				aList.add(temp);
-			}
-			st.close();
-			rs.close();
-			
-		} catch (SQLException e) {
-			logger.error("",e);
-		}
-		finally {
-			JDBChelper.close(rs);
-			JDBChelper.close(st);
-			JDBChelper.close(conn);
-			
-		}
-			if(found == true)
-				return aList.toArray(new NAPTRRecord[aList.size()]);
-			else
-				return null;
-	}
 	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
 	
 	public static String bytesToHex(byte[] bytes) {
