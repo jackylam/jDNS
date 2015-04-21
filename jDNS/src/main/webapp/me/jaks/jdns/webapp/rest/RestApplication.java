3e4688c28c1fae51bfbe5a6a6feb9ad3f1f94cf8
Jacky Lam <jwhlam@gmail.com>
2015-04-21 11:05:15 -0600

    Switching to Jersey 1.19

--- jDNS/src/main/webapp/me/jaks/jdns/webapp/rest/RestApplication.java
+++ jDNS/src/main/webapp/me/jaks/jdns/webapp/rest/RestApplication.java
@@ -1,10 +1,42 @@
 package me.jaks.jdns.webapp.rest;
 
-import org.glassfish.jersey.server.ResourceConfig;
+import me.jaks.jdns.webapp.RecordDao;
+import me.jaks.jdns.webapp.RecordDaoInt;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+
+/*
 //@ApplicationPath("rest")
 public class RestApplication extends ResourceConfig {
     public RestApplication() {
         packages("me.jaks.jdns.rest");
     }
 }
+*/
+//(1) Extend GuiceServletContextListener
+
+public class RestApplication extends GuiceServletContextListener {
+
+	 @Override
+	 protected Injector getInjector() {
+	 return Guice.createInjector( new ServletModule() {
+	 @Override
+	 protected void configureServlets() {
+	 
+		 bind(RecordDaoInt.class).to(RecordDao.class);
+		 ResourceConfig rc = new PackagesResourceConfig( "me.jaks.jdns.webapp" );
+		 for ( Class<?> resource : rc.getClasses() ) {
+			 bind( resource );
+		 }
+	 serve( "/rest/*" ).with( GuiceContainer.class );
+	 }
+	 } );
+	 }
+	}
